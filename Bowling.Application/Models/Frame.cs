dfe8da2ab9e0ddd4cefa2908d8419c447e442eef
Jack Hoyle <jack.hoyle@jet2.com>
2023-02-16 13:36:49 +0000

    Refactor, move Frame calculation logic to Frames and introduce "NextFrame" prop on Frame.

--- Bowling.Application/Models/Frame.cs
+++ Bowling.Application/Models/Frame.cs
@@ -1,15 +1,68 @@
-﻿namespace Bowling.Application.Models;
+﻿using System.Collections;
 
-public record Frame
+namespace Bowling.Application.Models;
+
+public record Frame : IEnumerable<Frame>
 {
     public IEnumerable<Roll> Rolls { get; set; }
-    public bool HasSpare { get; set; }
-    public bool HasStrike { get; set; }
+    public bool HasSpare => Rolls.Last().IsSpare;
+    public bool HasStrike => Rolls.First().IsStrike;
     public int Score => HasSpare || HasStrike ? 10 : Rolls.Sum(m => m.Value);
-    public Frame(string frame)
+    public Frame? NextFrame { get; set; }
+    public Frame(Queue<string> frameQueue)
     {
+        var frame = frameQueue.Dequeue();
         Rolls = frame.Select(m => new Roll(m));
-        HasSpare = Rolls.Last().IsSpare;
-        HasStrike = Rolls.First().IsStrike;
+        if (frameQueue.Count != 0)
+            NextFrame = new Frame(frameQueue);
+    }
+
+    public int CalculateScore()
+    {
+        var totalScore = Score;
+        if (HasSpare)
+        {
+            return totalScore + NextFrame.Rolls.First().Value;
+        }
+
+        if (HasStrike)
+        {
+            return CalculateStrikeScore(totalScore);
+        }
+
+        return totalScore;
+    }
+
+    private int CalculateStrikeScore(int totalScore)
+    {
+        var nextFrame = NextFrame;
+
+        if (nextFrame is null) 
+            return totalScore;
+        totalScore += nextFrame.Score;
+
+        if (nextFrame.HasStrike)
+        {
+            var secondFrame = nextFrame.NextFrame;
+            totalScore += secondFrame?.Rolls.First().Value ?? 0;
+                
+        }
+
+        return totalScore;
+    }
+
+    public IEnumerator<Frame> GetEnumerator()
+    {
+        var head = this;
+        do
+        {
+            yield return head;
+            head = head.NextFrame;
+        } while (head is not null);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
     }
 }
--- Bowling.Application/Models/Game.cs
+++ Bowling.Application/Models/Game.cs
@@ -2,75 +2,28 @@
 {
     public class Game
     {
-        public LinkedList<Frame>? Frames { get; set; }
+        public Frame Frame { get; set; }
 
         public Game(string scoreSheet)
         {
-            Frames = ToFrames(scoreSheet);
+            Frame = ToFrames(scoreSheet);
         }
 
         public int CalculateTotalScore()
         {
-            var totalScore = 0;
-            var frames = new LinkedList<Frame>(Frames ?? throw new InvalidOperationException("No Frames found"));
-            var frameNode = frames.First;
-            do
+            int sum = 0;
+            foreach (var currentFrame in Frame)
             {
-                if (frameNode.Value.HasSpare)
-                {
-                    totalScore += CalculateFrameWithSpare(frameNode.Next.Value);
-                }
-                else if (frameNode.Value.HasStrike)
-                {
-                    totalScore += CalculateTotalScoreWithStrike(frameNode);
-                }
-                else
-                {
-                    totalScore += frameNode.Value.Score;
-                }
+                sum += currentFrame.CalculateScore();
+            }
 
-                frameNode = frameNode.Next;
-            } while (frameNode is not null);
-
-            return totalScore;
+            return sum;
         }
 
-        private int CalculateTotalScoreWithStrike(LinkedListNode<Frame> frame)
+        private Frame ToFrames(string scoreSheet)
         {
-            var totalScore = frame.Value.Score;
-            var nextFrame = frame.Next;
-
-            if (nextFrame is null) 
-                return totalScore;
-
-            totalScore += nextFrame.Value.Score;
-
-            if (!nextFrame.Value.HasStrike)
-                return totalScore;
-
-            var secondFrame = nextFrame.Next;
-
-            if (secondFrame is null)
-                return totalScore;
-
-            totalScore += secondFrame.Value.Rolls.First().Value;
-
-
-            return totalScore;
-        }
-
-        private int CalculateFrameWithSpare(Frame nextFrame)
-        {
-            var nextRoll = nextFrame.Rolls.First().Value;
-            var frameScore = nextRoll + 10;
-
-            return frameScore;
-        }
-
-        private LinkedList<Frame> ToFrames(string scoreSheet)
-        {
-            var frames = scoreSheet.Split(" ").Select(m => new Frame(m));
-            return new LinkedList<Frame>(frames);
+            var frames = new Queue<string>(scoreSheet.Split(" "));
+            return new Frame(frames);
         }
     }
 }
